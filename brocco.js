d8e30fe6e4982b3ad3d2c515a89ff139df26e58f
Atul Varma <varmaa@gmail.com>
2012-12-06 18:12:42 -0500

    rearranged source code for better readability, similar to docco.coffee.

--- brocco.js
+++ brocco.js
@@ -50,130 +50,8 @@
 var Brocco = (function() {
   var version = "0.1.0";
   
-  // Originally, this was in a separate JSON file, but we're including
-  // it inline for simplicity.
-  var languages = {
-    ".coffee" :
-      {"name" : "coffee-script", "symbol" : "#"},
-    ".rb":
-      {"name" : "ruby", "symbol" : "#"},
-    ".py":
-      {"name": "python", "symbol" : "#"},
-    ".feature":
-      {"name" : "gherkin", "symbol" : "#"},
-    ".yaml":
-      {"name" : "yaml", "symbol" : "#"},
-    ".tex":
-      {"name" : "tex", "symbol" : "%"},
-    ".latex":
-      {"name" : "tex", "symbol" : "%"},
-    ".js":
-      {"name" : "javascript", "symbol" : "//"},
-    ".c":
-      {"name" : "c", "symbol" : "//"},
-    ".h":
-      {"name" : "c", "symbol" : "//"},
-    ".cpp":
-      {"name" : "cpp", "symbol" : "//"},
-    ".php":
-      {"name" : "php", "symbol" : "//"},
-    ".hs":
-      {"name" : "haskell", "symbol" : "--"},
-    ".erl":
-      {"name" : "erlang", "symbol" : "%"},
-    ".hrl":
-      {"name" : "erlang", "symbol" : "%"}
-  };
-  var highlightStart = '<div class="highlight"><pre>';
-  var highlightEnd = '</pre></div>';
+  // ## Main Documentation Generation Functions
   
-  // This is a stand-in for node's `path` module.
-  var path = {
-    basename: function(p, ext) {
-      var lastPart = p.split('/').slice(-1)[0];
-      if (ext)
-        lastPart = lastPart.slice(0, -(ext.length));
-      return lastPart;
-    },
-    extname: function(filename) {
-      return '.' + filename.split('.').slice(-1)[0];
-    }
-  };
-
-  // This default syntax highlighter really doesn't do any
-  // syntax highlighting at all; it just plops the plain-text
-  // source code in a `<pre>` element.
-  function defaultHighlighter(language, code, cb) {
-    // We'll leverage the DOM to do HTML escaping for us.
-    var div = document.createElement('div');
-    div.appendChild(document.createTextNode(code));
-    var result = '<div class="highlight"><pre>' +
-                 div.innerHTML +
-                 '</pre></div>\n';
-    cb(result, language.dividerText);
-  }
-  
-  // This default template produces an identical DOM to the 
-  // [docco.jst][] template used by Docco for single-source files. It's just
-  // easier to inline it than grab it via XHR because it complicates
-  // the use and deployment of this browser-side script.
-  //
-  //   [docco.jst]: https://github.com/jashkenas/docco/blob/master/resources/docco.jst
-  function defaultTemplate(context) {
-    function el(name, attrs, children) {
-      var element = document.createElement(name);
-      Object.keys(attrs).forEach(function(attr) {
-        element.setAttribute(attr, attrs[attr]);
-      });
-      (children || []).forEach(function(child) {
-        if (typeof(child) == "string") {
-          var temp = document.createElement("div");
-          temp.innerHTML = child;
-          for (var i = 0; i < temp.childNodes.length; i++)
-            element.appendChild(temp.childNodes[i]);
-        } else
-          element.appendChild(child);
-      });
-      return element;
-    }
-
-    return el("div", {}, [
-      el("div", {id: "container"}, [
-        el("div", {id: "background"}),
-        el("table", {cellpadding: 0, cellspacing: 0}, [
-          el("thead", {}, [
-            el("tr", {}, [
-              el("th", {"class": "docs"}, [el("h1", {}, [context.title])]),
-              el("th", {"class": "code"})
-            ])
-          ]),
-          el("tbody", {}, context.sections.map(function(section, i) {
-            return el("tr", {id: "section-" + (i+1)}, [
-              el("td", {"class": "docs"}, [
-                el("div", {"class": "pilwrap"}, [
-                  el("a", {
-                    "class": "pilcrow",
-                    "href": "#section-" + (i+1)
-                  }, ["&#182;"])
-                ]),
-                section.docsHtml
-              ]),
-              el("td", {"class": "code"}, [section.codeHtml])
-            ]);
-          }))
-        ])
-      ])
-    ]).innerHTML;
-  }
-  
-  // Retrieve the given file over XHR.
-  function getFile(path, cb) {
-    var req = new XMLHttpRequest();
-    req.open("GET", path);
-    req.onload = function() { cb(req.responseText); };
-    req.send(null);
-  }
-
   // Generate the documentation for a source file by (optionally) reading it
   // in, splitting it up into comment/code sections, highlighting them for
   // the appropriate language, and merging them into an HTML template.
@@ -292,6 +170,132 @@ var Brocco = (function() {
     });
   };
 
+  // ## Helpers & Setup
+  
+  // Originally, this was in a separate JSON file, but we're including
+  // it inline for simplicity.
+  var languages = {
+    ".coffee" :
+      {"name" : "coffee-script", "symbol" : "#"},
+    ".rb":
+      {"name" : "ruby", "symbol" : "#"},
+    ".py":
+      {"name": "python", "symbol" : "#"},
+    ".feature":
+      {"name" : "gherkin", "symbol" : "#"},
+    ".yaml":
+      {"name" : "yaml", "symbol" : "#"},
+    ".tex":
+      {"name" : "tex", "symbol" : "%"},
+    ".latex":
+      {"name" : "tex", "symbol" : "%"},
+    ".js":
+      {"name" : "javascript", "symbol" : "//"},
+    ".c":
+      {"name" : "c", "symbol" : "//"},
+    ".h":
+      {"name" : "c", "symbol" : "//"},
+    ".cpp":
+      {"name" : "cpp", "symbol" : "//"},
+    ".php":
+      {"name" : "php", "symbol" : "//"},
+    ".hs":
+      {"name" : "haskell", "symbol" : "--"},
+    ".erl":
+      {"name" : "erlang", "symbol" : "%"},
+    ".hrl":
+      {"name" : "erlang", "symbol" : "%"}
+  };
+  var highlightStart = '<div class="highlight"><pre>';
+  var highlightEnd = '</pre></div>';
+  
+  // This is a stand-in for node's `path` module.
+  var path = {
+    basename: function(p, ext) {
+      var lastPart = p.split('/').slice(-1)[0];
+      if (ext)
+        lastPart = lastPart.slice(0, -(ext.length));
+      return lastPart;
+    },
+    extname: function(filename) {
+      return '.' + filename.split('.').slice(-1)[0];
+    }
+  };
+
+  // This default syntax highlighter really doesn't do any
+  // syntax highlighting at all; it just plops the plain-text
+  // source code in a `<pre>` element.
+  function defaultHighlighter(language, code, cb) {
+    // We leverage the DOM to do HTML escaping for us.
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(code));
+    var result = '<div class="highlight"><pre>' +
+                 div.innerHTML +
+                 '</pre></div>\n';
+    cb(result, language.dividerText);
+  }
+  
+  // This default template produces an identical DOM to the 
+  // [docco.jst][] template used by Docco for single-source files. It's just
+  // easier to inline it than grab it via XHR because it complicates
+  // the use and deployment of this browser-side script.
+  //
+  //   [docco.jst]: https://github.com/jashkenas/docco/blob/master/resources/docco.jst
+  function defaultTemplate(context) {
+    function el(name, attrs, children) {
+      var element = document.createElement(name);
+      Object.keys(attrs).forEach(function(attr) {
+        element.setAttribute(attr, attrs[attr]);
+      });
+      (children || []).forEach(function(child) {
+        if (typeof(child) == "string") {
+          var temp = document.createElement("div");
+          temp.innerHTML = child;
+          for (var i = 0; i < temp.childNodes.length; i++)
+            element.appendChild(temp.childNodes[i]);
+        } else
+          element.appendChild(child);
+      });
+      return element;
+    }
+
+    return el("div", {}, [
+      el("div", {id: "container"}, [
+        el("div", {id: "background"}),
+        el("table", {cellpadding: 0, cellspacing: 0}, [
+          el("thead", {}, [
+            el("tr", {}, [
+              el("th", {"class": "docs"}, [el("h1", {}, [context.title])]),
+              el("th", {"class": "code"})
+            ])
+          ]),
+          el("tbody", {}, context.sections.map(function(section, i) {
+            return el("tr", {id: "section-" + (i+1)}, [
+              el("td", {"class": "docs"}, [
+                el("div", {"class": "pilwrap"}, [
+                  el("a", {
+                    "class": "pilcrow",
+                    "href": "#section-" + (i+1)
+                  }, ["&#182;"])
+                ]),
+                section.docsHtml
+              ]),
+              el("td", {"class": "code"}, [section.codeHtml])
+            ]);
+          }))
+        ])
+      ])
+    ]).innerHTML;
+  }
+  
+  // Retrieve the given file over XHR.
+  function getFile(path, cb) {
+    var req = new XMLHttpRequest();
+    req.open("GET", path);
+    req.onload = function() { cb(req.responseText); };
+    req.send(null);
+  }
+  
   // Get the current language we're documenting, based on the extension.
   function getLanguage(source) {
     return languages[path.extname(source)];
