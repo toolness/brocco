5978288352209573c9a3381865fb7f48ac3990c1
Atul Varma <varmaa@gmail.com>
2012-12-10 00:13:21 -0500

    slightly more readable tests.

--- test/test-html-and-css.js
+++ test/test-html-and-css.js
@@ -10,22 +10,32 @@
   }
   
   test("works when /* */ comment follows // comment", function() {
-    var code = '<script>// hi\n/* there */</' + 'script>';
-    deepEqual(makeSections(".html", code).map(function(section) {
+    deepEqual(makeSections(".html", [
+      '<script>// hi',
+      '/* there */</script>'
+    ].join('\n')).map(function(section) {
       return section.docsText;
     }), ['', 'hi\n', 'there ']);
   });
   
   test("works with HTML comments", function() {
-    var code = '<br><!-- hi\n    there -->\n<p>sup</p>';
-    deepEqual(makeSections(".html", code).map(function(section) {
+    deepEqual(makeSections(".html", [
+      '<br>',
+      '<!-- hi',
+      '     there -->',
+      '<p>sup</p>'
+    ].join('\n')).map(function(section) {
       return section.docsText;
     }), ['', 'hi\nthere ']);
   });
   
   test("works w/ .css files", function() {
-    var code = 'body {\n  /* cool */\n  color: blue;\n}\n';
-    deepEqual(makeSections(".css", code).map(function(section) {
+    deepEqual(makeSections(".css", [
+      'body {',
+      '  /* cool */',
+      '  color: blue;',
+      '}'
+    ].join('\n')).map(function(section) {
       return section.docsText;
     }), ['', 'cool ']);
   });
