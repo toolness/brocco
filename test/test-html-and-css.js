c239d21de1f65dd717c24c288ff7122a5a06a6a7
Atul Varma <varmaa@gmail.com>
2012-12-10 00:09:40 -0500

    more tests

--- test/test-html-and-css.js
+++ test/test-html-and-css.js
@@ -15,4 +15,18 @@
       return section.docsText;
     }), ['', 'hi\n', 'there ']);
   });
+  
+  test("works with HTML comments", function() {
+    var code = '<br><!-- hi\n    there -->\n<p>sup</p>';
+    deepEqual(makeSections(".html", code).map(function(section) {
+      return section.docsText;
+    }), ['', 'hi\nthere ']);
+  });
+  
+  test("works w/ .css files", function() {
+    var code = 'body {\n  /* cool */\n  color: blue;\n}\n';
+    deepEqual(makeSections(".css", code).map(function(section) {
+      return section.docsText;
+    }), ['', 'cool ']);
+  });
 })();
